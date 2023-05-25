69a29d86fddf1ad24528907a1db82278499759e0
정영우 <117256289+jay3614@users.noreply.github.com>
2023-05-25 15:54:17 +0900

    광고배너 이미지 변경

--- front-end/src/components/RightSidebar.js
+++ front-end/src/components/RightSidebar.js
@@ -11,7 +11,7 @@ const ImageBanner = () => {
 
   return (
     <Box position="fixed" width={250} style={{ marginLeft: "8em" }}>
-      {isVisible && <a href="https://ujb.greenart.co.kr" target="_blank" rel="noreferrer"><img src={process.env.PUBLIC_URL + '/banner.png'} alt="광고문의는 000으로" className="blink"/></a>}
+      {isVisible && <a href="https://ujb.greenart.co.kr" target="_blank" rel="noreferrer"><img src={process.env.PUBLIC_URL + '/banner.jpg'} alt="광고문의는 000으로" className="blink"/></a>}
       {isVisible && (<IconButton sx={{ position: "absolute", top: 0, right: 0 }} onClick={handleClose}><CloseIcon /></IconButton>)}
     </Box>
   );
